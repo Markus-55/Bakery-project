470cb2420821ac2f37b4fd59399aba62bd86ae6d
Ghionii <148452291+Ghionii@users.noreply.github.com>
2023-12-20 16:58:20 +0100

    added classes to the HTML and "addToCart" in Javascript

--- assets/kundvagn.js
+++ assets/kundvagn.js
@@ -6,13 +6,18 @@ if (document.readyState == 'loading') {
 
 function ready() {
   const removeCartItemsButton = document.querySelectorAll('.remove-item');
-  console.log(removeCartItemsButton);
   for (let i = 0; i < removeCartItemsButton.length; i++) {
     let button = removeCartItemsButton[i];
     button.addEventListener('click', RemoveCartItem)
   }
 
 }
+
+const addToCartButton = document.querySelectorAll(".shop-item-button")
+for (let i = 0; i < addToCartButton.length; i++) {
+  let button = addToCartButton[i];
+  button.addEventListener("click", addToCartClicked)
+} 
 
 const quantityInputs = document.querySelectorAll(".cart-quantity")
 for (let i = 0; i < quantityInputs.length; i++) {
@@ -21,7 +26,7 @@ for (let i = 0; i < quantityInputs.length; i++) {
 }
 function RemoveCartItem(event) {
     let buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
+    buttonClicked.parentElement.parentElement.remove();
     updateCartTotal();
 }
 
@@ -34,6 +39,31 @@ function quantityChanged(event) {
 
 }
 
+function addToCartClicked(event) {
+  let button = event.target
+  let shopItem = button.parentElement.parentElement;
+  let title = shopItem.querySelector(".shop-item-title").innerText;
+  let price = shopItem.querySelector(".shop-item-price").innerText;
+  addItemToCart(title, price)
+  updateCartTotal()
+}
+
+function addItemToCart(title, price){
+  let cartRow = document.createElement("div")
+  let cartItems = document.querySelector(".cart-items")
+  let cartRowContents = `
+        <div class="cart-row">
+        <span class="cart-item-title">${title}</span>
+          <input class="cart-quantity" type="number" value="1" />
+          <span class="item-price">${price}</span>
+          <button class="remove-item">Ta bort</button>
+        </div>`
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+  cartRow.querySelector(".remove-item").addEventListener("click", RemoveCartItem)
+  cartRow.querySelector(".cart-quantity").addEventListener("change", quantityChanged)
+}
+
 function updateCartTotal() {
   let cartRows = document.querySelectorAll('.cart-row');
   let total = 0;
@@ -44,7 +74,7 @@ function updateCartTotal() {
     let price = parseFloat(priceElement.innerText.replace('kr', ' '));
     let quantity = quantityElement.value;
     console.log(price * quantity);
-    total = total + price * quantity;
+    total = total + (price * quantity)
   }
-  document.querySelector('.total-price').innerText = total + 'kr';
+  document.querySelector('.cart-total-price').innerText = total + 'kr';
 }
