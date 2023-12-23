9f6dd0c7e88fd25c97a4c03c94439d31eff2619f
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2023-12-23 01:03:45 +0100

    Feat: match date with correct week day

--- assets/calendar.js
+++ assets/calendar.js
@@ -24,24 +24,33 @@ const months = [
 // console.log(months[currMonth]);
 
 const renderCalendar = () => {
-  const datesOfCurrMonth = [];
+  const allDatesOfMonth = [];
+  const firstDayOfMonth = new Date(currYear, currMonth, 0).getDay();
   const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
-  const lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth);
-  
-  document.querySelector('.current-date').innerText = `${months[currMonth]} ${currYear}`;
-  
-  
-  for(let date = 1; date <= lastDateOfMonth; date++) {
-    datesOfCurrMonth.push(date);
+  const lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+
+  //pushes last dates of last month to allDatesOfMonth arr
+  for(let i = firstDayOfMonth; i > 0; i--) {
+    console.log(1, i);
+    allDatesOfMonth.push(lastDateOfLastMonth - i + 1);
+    console.log(lastDateOfLastMonth - i + 1, lastDateOfLastMonth - i, i, 1);
   }
   
-  //for loop that prints all day of month
-  for(let i = 0; i <= datesOfCurrMonth.length -1; i++) {
+  //pushes dates of curr month to allDatesOfMonth arr
+  for(let i = 1; i <= lastDateOfMonth; i++) {
+    allDatesOfMonth.push(i);
+  }
+  
+  //displays & appends allDatesOfMonth to calendar
+  for(let i = 0; i <= allDatesOfMonth.length - 1; i++) {
     const li = document.createElement('li');
-    li.append(datesOfCurrMonth[i]);
-    document.querySelector('.days').appendChild(li);    
+    li.append(allDatesOfMonth[i]);
+    
+    document.querySelector('.days').appendChild(li);
   }
+
+  document.querySelector('.current-date').innerText = `${months[currMonth]} ${currYear}`;
 }
 
 renderCalendar();
-                       
+    
