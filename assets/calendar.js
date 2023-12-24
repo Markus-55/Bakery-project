0d69c507e86cd87ed7b1644c8fa3f186487cec13
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2023-12-24 10:28:47 +0100

    Feat: add next month first days

--- assets/calendar.js
+++ assets/calendar.js
@@ -20,32 +20,33 @@ const months = [
   'December'
 ];
 
-// console.log(currMonth, currDate, currYear);
-// console.log(months[currMonth]);
-
 const renderCalendar = () => {
   const allDatesOfMonth = [];
   const firstDayOfMonth = new Date(currYear, currMonth, 0).getDay();
   const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
   const lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+  const lastDayOfMonth = new Date(currYear, currMonth + 1, 0).getDay();
 
-  //pushes last dates of last month to allDatesOfMonth arr
+  // pushes last dates of last month to allDatesOfMonth arr
   for(let i = firstDayOfMonth; i > 0; i--) {
-    console.log(1, i);
-    allDatesOfMonth.push(lastDateOfLastMonth - i + 1);
-    console.log(lastDateOfLastMonth - i + 1, lastDateOfLastMonth - i, i, 1);
+    allDatesOfMonth.push((lastDateOfLastMonth - i + 1));
   }
   
-  //pushes dates of curr month to allDatesOfMonth arr
+  // pushes dates of curr month to allDatesOfMonth arr
   for(let i = 1; i <= lastDateOfMonth; i++) {
     allDatesOfMonth.push(i);
+  }
+  
+  // pushes first date of next month to allDatesOfMonth arr
+  for(let i = lastDayOfMonth; i < 7; i++) {
+    allDatesOfMonth.push(i - lastDayOfMonth + 1);
   }
   
   //displays & appends allDatesOfMonth to calendar
   for(let i = 0; i <= allDatesOfMonth.length - 1; i++) {
     const li = document.createElement('li');
-    li.append(allDatesOfMonth[i]);
     
+    li.append(allDatesOfMonth[i]);
     document.querySelector('.days').appendChild(li);
   }
 
