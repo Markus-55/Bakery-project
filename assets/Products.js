2692e318627361569d621783f48a17924ecbe080
Ghionii <148452291+Ghionii@users.noreply.github.com>
2023-12-26 21:17:53 +0100

    La till "type" i objekten(produkterna)

--- assets/Products.js
+++ assets/Products.js
@@ -2,6 +2,7 @@ const products = [
   {
   id: 0,
   name: "Prinsses Tårta",
+  type: "tårta",
   price: 250,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/princess-cake.jpg",
@@ -9,6 +10,7 @@ const products = [
 {
   id: 1,
   name: "Choklad Tårta",
+  type: "tårta",
   price: 300,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/princess-cake.jpg",
@@ -16,6 +18,7 @@ const products = [
 {
   id: 2,
   name: "Frukt Tårta",
+  type: "tårta",
   price: 300,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/fruitcake.jpg",
@@ -23,6 +26,7 @@ const products = [
 {
   id: 3,
   name: "Lussekatt",
+  type: "bakelse",
   price: 20,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/lussekatt.jpg",
@@ -30,6 +34,7 @@ const products = [
 {
   id: 4,
   name: "Rågbröd",
+  type: "bröd",
   price: 30,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/rågbröd.jpg",
@@ -37,6 +42,7 @@ const products = [
 {
   id: 5,
   name: "Baguette",
+  type: "bröd",
   price: 20,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/baguette.jpg",
@@ -44,6 +50,7 @@ const products = [
 {
   id: 6,
   name: "Levain bröd",
+  type: "bröd",
   price: 30,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/levainbröd.jpg",
@@ -51,6 +58,7 @@ const products = [
 {
   id: 7,
   name: "Kanelbulle",
+  type: "bakelse",
   price: 15,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/kanelbulle.jpg",
@@ -58,6 +66,7 @@ const products = [
 {
   id: 8,
   name: "Semla",
+  type: "bakelse",
   price: 30,
   description: "Lorem ipsum dolor sit amet.",
   imagesrc: "bakelser/semla.jpg",
